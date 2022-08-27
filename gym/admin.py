e749f23a1b740bb52b3ea4b9725c24ad8e1cfecd
Allan Stocco Rezende <allanstocco@hotmail.com>
2022-08-27 14:40:27 +0100

    Refresh App

--- gym/admin.py
+++ gym/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.forms import TextInput, Textarea, CharField
 from .models import *
 # Register your models here.
 
 
 admin.site.register(UserProfile)
-admin.site.register(UserWorkout)
 admin.site.register(Exercises)
 admin.site.register(UserWorkoutSession)
+admin.site.register(UserWorkout)
--- gym/models.py
+++ gym/models.py
@@ -13,22 +13,26 @@ class UserProfile(models.Model):
 
 
 class UserWorkout(models.Model):
+    user_workout_id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
-    user_workout_session = models.ForeignKey(
-        'UserWorkoutSession', on_delete=models.CASCADE, related_name='workout_session', related_query_name='workout_session', null=True, blank=True)
+    user_workout_description = models.CharField(max_length=150, null=True, blank=True)
+    
+    def __str__(self):
+        return f'{self.user} Workout'
 
 
 class Exercises(models.Model):
     exercise = models.CharField(max_length=60, null=True, blank=True)
-
+    exercise_description = models.CharField(max_length=512, null=True, blank=True)
+    
     def __str__(self):
         return self.exercise
 
 
 class UserWorkoutSession(models.Model):
     user_workout_id = models.ForeignKey(
-        UserWorkout, on_delete=models.CASCADE, related_name='comments', related_query_name='comment', null=True, blank=True)
+        UserWorkout, on_delete=models.CASCADE, related_name='user_workout', related_query_name='user_workout', null=True, blank=True)
     exercise = models.ForeignKey(
         Exercises, on_delete=models.CASCADE, related_name='exercises', related_query_name='exercises')
     exercise_reps = models.IntegerField(default=0, blank=True, null=True)
