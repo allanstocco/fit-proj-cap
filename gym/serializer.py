5edc300fafb47f125c29d695c1bf32b544982cf8
Allan Stocco Rezende <allanstocco@hotmail.com>
2022-08-31 21:37:39 +0100

    next

--- gym/serializer.py
+++ gym/serializer.py
@@ -17,8 +17,8 @@ class ExerciseSetSerializer(serializers.ModelSerializer):
 
 
 class WorkoutExerciseSessionSerializer(serializers.ModelSerializer):
-    workout_exercise_set = ExerciseSetSerializer(many=True)
-    exercise_name = serializers.CharField(source='exercise.exercise')
+    workout_exercise_set = ExerciseSetSerializer(read_only=True, many=True)
+    exercise_name = serializers.CharField(read_only=True, source='exercise.exercise')
 
     class Meta:
         model = WorkoutExerciseSession
